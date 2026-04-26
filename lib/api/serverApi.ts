39dff57aeef2b21adca6d3f2350504af032ea1ba
Karina Bosenko <karina.bosenko.10.10@gmail.com>
2026-04-26 09:32:49 +0300

    Fix serverApi

--- lib/api/serverApi.ts
+++ lib/api/serverApi.ts
@@ -1,5 +1,5 @@
 import { cookies } from "next/headers";
-import { nextServer } from "./api";
+import nextServer from "./api";
 import { User } from "@/types/user";
 import { Note } from "@/types/note";
 
