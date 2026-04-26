47b129ba58c784d8ad6989f6f51f98583ea0a432
Karina Bosenko <karina.bosenko.10.10@gmail.com>
2026-04-26 09:28:32 +0300

    Fix noteStore

--- lib/store/noteStore.ts
+++ lib/store/noteStore.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
-import { CreateNote } from "../api/clientApi";
+import type { CreateNote } from "../api/clientApi";
 
 type NoteDraftStore = {
   draft: CreateNote;
